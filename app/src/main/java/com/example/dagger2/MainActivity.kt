a0a0e7b2fe71ee8f438dd2a77152d15c2c6ccbef
Egor Kisel <kisel1989@mail.ru>
2024-01-10 19:28:43 +0300

    @AssistedInject, @Assisted, @AssistedFactory example

--- app/src/main/java/com/example/dagger2/MainActivity.kt
+++ app/src/main/java/com/example/dagger2/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.dagger2
 
 import android.os.Bundle
+import androidx.lifecycle.ViewModel
 import com.example.dagger2.di.AppComponent
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
 import javax.inject.Inject
 
 class MainActivity : BaseActivity<MainViewModel>() {
 
-    @Inject lateinit var deps: Deps
+    @Inject
+    lateinit var deps: Deps
+    @Inject
+    lateinit var secondViewModelFactory: SecondViewModel.Factory
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,9 +21,23 @@ class MainActivity : BaseActivity<MainViewModel>() {
         AppComponent.create(applicationContext).inject(this)
         deps
         viewModel.test()
+
+        val viewModel2 = secondViewModelFactory.create("123")
     }
 }
 
+class SecondViewModel @AssistedInject constructor(
+    @Assisted val id: String,
+    val repoImpl: RepoImpl,
+) {
+    @AssistedFactory
+    interface Factory {
+        fun create(id: String): SecondViewModel
+    }
+}
+
+class RepoImpl @Inject constructor()
+
 data class Deps(
-    val name: String
+    val name: String,
 )
